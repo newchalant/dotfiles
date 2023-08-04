db3620de64fb846971ee45824f2c62402d5c6b1a
newchalant <138784016+newchalant@users.noreply.github.com>
2023-08-04 18:52:56 +0300

    Update install.sh

--- rust/install.sh
+++ rust/install.sh
@@ -5,7 +5,7 @@
 
 if ! [ -x "$(command -v rustup)" ]; then
   echo "  Installing Rust for you."
-  yes y | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 #else
 #   rustup update
 fi
