be76ea227127d0567c0b7425a103c0cf3f9580ad
newchalant <138784016+newchalant@users.noreply.github.com>
2023-08-04 17:35:02 +0300

    Update install.sh

--- lvim/install.sh
+++ lvim/install.sh
@@ -3,8 +3,7 @@
 # Installs LunarVim
 #
 
-if test ! $(which lvim)
-then
+if ! [ -x "$(command -v lvim)" ]; then
   echo "  Installing LunarVim for you."
   yes yes | LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 fi
